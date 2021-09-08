0e2fcea7632b61ce3d2b0b6d58ff605d48b4042b
Sebastian Lauwers <sl@mailicio.us>
2021-09-08 23:11:11 +0200

    Remove full stops in help message

--- src/bot/commands/meta.rs
+++ src/bot/commands/meta.rs
@@ -18,9 +18,9 @@ async fn help(ctx: &Context, msg: &Message) -> CommandResult {
     let message = "Hi, I'm liro!\n\
                    I help automate role assignments based on your rating. To get started, please \
                    link your lichess account to your Discord user by saying `ohnomy \
-                   account`.\n\
+                   account`\n\
                    After that, you can ask me to calculate your average rating and update your \
-                   Discord role if necessary by saying `ohnomy rating`.";
+                   Discord role if necessary by saying `ohnomy rating`";
     msg.channel_id.say(&ctx.http, message).await?;
 
     Ok(())

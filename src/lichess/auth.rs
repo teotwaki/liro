cc22beb330a67474843a15dfa6a199d871ec59de
Sebastian Lauwers <sl@mailicio.us>
2021-09-14 22:17:28 +0200

    Move link generation to lichess module

--- src/lichess/auth.rs
+++ src/lichess/auth.rs
@@ -39,3 +39,19 @@ pub async fn fetch_access_token(code: &str, code_verifier: &str) -> Result<Strin
         Err(UnexpectedStatusError)
     }
 }
+
+pub fn oauth_url(code_challenge: &str, state: &str) -> String {
+    format!(
+        "https://lichess.org/oauth\
+             ?response_type=code\
+             &redirect_uri={}/oauth/callback\
+             &client_id={}\
+             &code_challenge_method=S256\
+             &code_challenge={}\
+             &state={}",
+        config::hostname(),
+        config::client_id(),
+        code_challenge,
+        state
+    )
+}
